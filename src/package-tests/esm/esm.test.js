243718618574313f0e5689fb4c20a3d3fd62e087
HP Dietz <hp@jandh.org>
2020-02-11 13:21:50 +0100

    Add CircleCI configuration. Add package test.

---
+++ src/package-tests/esm/esm.test.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Console from 'react-component-console';
+
+describe('esm module test', () => {
+  let container;
+
+  beforeEach(() => {
+    container = document.createElement('div');
+  });
+
+  it('renders without crashing', () => {
+    ReactDOM.render(
+      <Console
+        lines={[
+          'ESM module test.'
+        ]}
+      />,
+      container);
+    ReactDOM.unmountComponentAtNode(container);
+  });
+});
